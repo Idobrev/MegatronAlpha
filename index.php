1593f9d359c7c6aad2b4e6ed42d5a2c4ef2ea3ff
Ivan Dobrev <i.nikolaev.d@gmail.com>
2016-03-13 10:19:16 +0200

    Initial Alpha

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$file = file_get_contents('index.html');
+preg_match_all('/megatron-module=(?:\'|\")(.*)(?:\'|\")/', $file, $matches);
+echo '<pre>';
+print_r($matches);
+echo '</pre>';
+#echo $file;
+#require $file;
+?>
